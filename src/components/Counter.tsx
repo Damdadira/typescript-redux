28635353b68991a0a5a9bae2e925b3bf574968fc
duddk <duddk@scatterx.io>
2024-10-21 17:45:53 +0900

    작업중

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -8,10 +8,10 @@ type CounterProps = {
 export default function Counter({count, onIncrease, onDecrease, onIncreaseBy}: CounterProps) {
   return(
     <div>
-      <p>{count}</p>
-      <button onClick={onIncrease}>+1</button>
-      <button onClick={onDecrease}>-1</button>
-      <button onClick={() => onIncreaseBy(5)}>+5</button>
+      <h1 style={{color: '#70943b'}}>{count}</h1>
+      <button style={{background: '#b8e07b', color: '#fff', marginRight: '0.5rem'}} onClick={onIncrease}>+1</button>
+      <button style={{background: '#9bbd68', color: '#fff', marginRight: '0.5rem'}} onClick={onDecrease}>-1</button>
+      <button style={{background: '#779150', color: '#fff', marginRight: '0.5rem'}} onClick={() => onIncreaseBy(5)}>+5</button>
     </div>
   )
 }
